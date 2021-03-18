dae5cb411b70b856183c7d69437f099ba6ee0df2
alxayeed <alxayeed@gmail.com>
2021-03-18 14:08:15 +0600

    postgres container configured as database

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -83,6 +83,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_blog_project',
+        'HOST': '172.17.0.2',
+        'PORT': '5432',
+        'USER': 'postgres'
     }
 }
 
