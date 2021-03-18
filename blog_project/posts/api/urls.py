307300020b8f8db2a3161780f89d1a15406295f9
alxayeed <alxayeed@gmail.com>
2021-03-18 16:55:45 +0600

    swagger docs added

--- blog_project/posts/api/urls.py
+++ blog_project/posts/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from posts.api import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
@@ -10,4 +11,10 @@ urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls",
                               namespace="rest_framework")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/", SpectacularSwaggerView.as_view(
+        template_name="posts/swagger-ui.html", url_name="schema"
+    ),
+        name="swagger-ui",
+    ),
 ]
